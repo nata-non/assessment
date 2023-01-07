12f449600367c9820125078e8c22069d01495ffa
nata-non <63070045@kmitl.ac.th>
2023-01-07 20:33:17 +0700

    feat: env and port

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/nata-non/assessment
+
+go 1.19
+
+require github.com/joho/godotenv v1.4.0 // indirect
--- server.go
+++ server.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	fmt.Println("Please use server.go for main file")
 	fmt.Println("start at port:", os.Getenv("PORT"))
 }
